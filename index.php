751c7aa79f7039504a4e597d316b086dd138627c
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-02-06 18:18:20 +0800

    Add index.php

---
+++ index.php
@@ -0,0 +1,151 @@
+<html>
+<head>
+<link rel="shortcut icon" href="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/27657057_864496250378076_845488150974365384_n.jpg?oh=eb2951db96d39f9a72e1508572a1149b&oe=5B1381B9" type="image/x-icon" />
+<title>Workshop | Cobacha</title>
+<style>
+div.head-bg {
+background-color: green;
+}
+
+div.main {
+font-family: 'Arial';
+margin-left: 120pt;
+margin-right: 120pt;
+}
+
+div.pd {
+width: 500px;
+}
+
+div.img {
+width: 500px;
+}
+
+div.foot {
+background-color: grey;
+text-align: center;
+font-family: 'Arial';
+color: white;
+}
+
+h1.head {
+font-family: 'Arial';
+color: white;
+text-align: center;
+}
+
+table {
+border: 1px solid gray;
+text-align: center;
+position: relatve;
+width: 1013px; 
+}
+
+th {
+border: 1px solid gray;
+color: white;
+background-color: green; 
+}
+
+td {
+border: 1px solid gray;
+}
+
+img.html {
+width: 40px;
+}
+
+img.mypic {
+position: absolute;
+width: 400px;
+margin-left: 315px;
+border: 10px solid green;
+}
+</style>
+</head>
+<body>
+<div class='head-bg'>
+<br>
+<h1 class='head'>PERSONAL INFORMATION</h1><hr>
+</div>
+<div class='main'>
+<br>
+<div>
+	<center><h2><u>BIOGRAPHY</u></h2></center>
+	<p>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I am Brandon Dwite Sugbo Cobacha, a Bachelor of Science in Information and Communication student of Cebu Technological University, Naga Extension Campus, 
+	City of Naga, Cebu. I am already 20 and I'm still single. I am presently residing in Central Poblacion, City of Naga, Cebu. Originally, I am a pure Waraynon 
+        and Brgy. Barbo Dulag, Leyte is my home address. I am the eldest among the children of Manuel Lumpas Cobacha Jr. and Josefina Sugbo Cobacha.<br><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I studied my primary grades from Kindergarten to Grade 3 in Barbo Primary School and transferred to Dulag Central School to finish my elementary grades.
+	I went to Dulag National High School to study my secondary level. When I was in 4th year high school, I passed the scholarship examination for ICOT-ICT Program.
+	While I was on that year level, I studied basic computer programming during Sundays at the same time. After graduating high school, I continued my computer programming
+	for Software Development NC IV.<br><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Before continuing my study, there was a tradegy happened. Year 2013, a great disaster named Typhoon Yolanda destroyed our lives and we were all affected.
+	Our house was vanished and nothing remained. It took about 6 months for us to recover.<br><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Years had past, I went to Cebu for job opportunities. I found a job at Wenbing Enterprises, City of Naga, Cebu. I worked as a salesman, a technician and a computer
+	operator daily. After all, I decided to take bachelor's degree for ICT in CTU Naga Extension Campus. Now I'm still studying and I'm nearly finish on my 1st year level.  
+	</p><hr>
+</div>
+<h3><u>PERSONAL DATA:</u></h3>
+<div class="pd">
+<p><b>Name: </b>Brandon Dwite Sugbo Cobacha<img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.0-9/24059009_830082143819487_3066666356779376484_n.jpg?oh=34a050fb44e0085553bee740199bea29&oe=5B1AD216" alt="My Pic" class="mypic"></p>
+<p><b>Address: </b>Brgy. Barbo Dulag, Leyte</p>
+<p><b>Age: </b>20 yrs. old</p>
+<p><b>Gender: </b>Male</p>
+<p><b>Height: </b>5'5"</p>
+<p><b>Weight: </b>44 kg.</p>
+<p><b>Civil Status: </b>Single</p>
+<p><b>Nationality: </b>Filipino</p>
+<p><b>Religion: </b>Roman Catholic</p>
+<p><b>Dialect Spoken: </b>Waray, Bisaya, Filipino & English</p>
+<p><b>Contact Number: </b>+639354919244</p>
+<p><b>Father's Name: </b>Manuel Lumpas Cobacha Jr.</p>
+<p><b>Mother's Name: </b>Josefina Sugbo Cobacha</p>
+</div>
+<div class="img">
+
+</div>
+<br>
+<hr>
+<h3><u>EDUCATIONAL BACKGROUND:</u></h3>
+<table>
+<tr>
+<th>Level</th>
+<th>School Name</th>
+<th>Address</th>
+<th>Academic Year</th>
+</tr>
+<tr>
+<td>College</td>
+<td>Cebu Technological University</td>
+<td>CTU-Naga Extension Campus, City of Naga, Cebu</td>
+<td>2017 - present</td>
+</tr>
+<tr>
+<td>Secondary</td>
+<td>Dulag National High School</td>
+<td>Dulag, Leyte</td>
+<td>2010 - 2014</td>
+</tr>
+<tr>
+<td>Elementary</td>
+<td>Dulag Central School</td>
+<td>Dulag, Leyte</td>
+<td>2007 - 2010</td>
+</tr>
+<tr>
+<td>Primary</td>
+<td>Barbo Primary School</td>
+<td>Barbo, Dulag, Leyte</td>
+<td>2003 - 2004</td>
+</tr>
+</table>
+<br>
+</div>
+<div class='foot'>
+<br>
+<p><a href="www.gmail.com">bdscobacha@gmail.com</a>.All Rights Reserved 2018.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>Powered by</i><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png" alt="HTML Logo" class="html"></p>
+<hr>
+</div>
+</body>
+</html>
